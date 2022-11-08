b7a4ea698163be96b945c4c5b92af3ac79ad6d75
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-08 19:02:18 +0600

    get specific service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -9,8 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb config
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjvalws.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -30,6 +29,17 @@ async function run() {
 
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    // get single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: ObjectId(id) };
+
+      const service = await servicesCollection.findOne(query);
+
+      res.send(service);
     });
   } finally {
     //
