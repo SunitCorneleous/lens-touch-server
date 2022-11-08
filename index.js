89e678c863f409a9ef88da3d91e7b05837c7db42
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-08 18:51:40 +0600

    get all services

--- index.js
+++ index.js
@@ -8,6 +8,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb config
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjvalws.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const servicesCollection = client.db("lensTouch").collection("services");
+
+    // get services
+    app.get("/services", async (req, res) => {
+      const limit = parseInt(req.query.limit);
+      const query = {};
+      const cursor = servicesCollection.find(query).limit(limit);
+
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    //
+  }
+}
+
+run().catch(err => console.log(err));
+
 // home route
 app.get("/", (req, res) => {
   res.send("<h1>Lens Touch server is running</h1>");
